2fa535280bb0d18192e07d27c1b9e7587b641186
Bryant Biggs <bryantbiggs@gmail.com>
2024-09-24 11:34:05 -0500

    chore: Update GitHub actions to latest for supported runtimes (#1153)
    * chore: Update GitHub actions to latest for supported runtimes
    
    * chore: Remove Helm v2 and update test component versions
    
    * chore: Remove `setup-go` action; go is not used

--- scripts/install-toolchain.sh
+++ scripts/install-toolchain.sh
@@ -10,11 +10,10 @@ TOOLS_DIR="${BUILD_DIR}/tools"
 mkdir -p "${TOOLS_DIR}"
 export PATH="${TOOLS_DIR}:${PATH}"
 
-HELMV2_VERSION="v2.17.0"
-HELMV3_VERSION="v3.6.3"
+HELM_VERSION="v3.16.1"
 KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-KIND_VERSION=v0.11.1
-BATS_VERSION=1.1.0
+KIND_VERSION=v0.24.0
+BATS_VERSION=1.11.0
 
 ## Install kubectl
 curl -sSL "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/${PLATFORM}/${ARCH}/kubectl" -o "${TOOLS_DIR}/kubectl"
@@ -25,26 +24,15 @@ mkdir -p "${TMP_DIR}/kubeval"
 curl -sSL https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-${PLATFORM}-${ARCH}.tar.gz | tar xz -C "${TMP_DIR}/kubeval"
 mv "${TMP_DIR}/kubeval/kubeval" "${TOOLS_DIR}/kubeval"
 
-## Install helm v2
-mkdir -p "${TMP_DIR}/helmv2"
-curl -sSL  https://get.helm.sh/helm-${HELMV2_VERSION}-${PLATFORM}-${ARCH}.tar.gz | tar xz -C "${TMP_DIR}/helmv2"
-mv "${TMP_DIR}/helmv2/${PLATFORM}-${ARCH}/helm" "${TOOLS_DIR}/helmv2"
-rm -rf "${PLATFORM}-${ARCH}"
-
-## Install helm v3
-mkdir -p "${TMP_DIR}/helmv3"
-curl -sSL https://get.helm.sh/helm-${HELMV3_VERSION}-${PLATFORM}-${ARCH}.tar.gz | tar xz -C "${TMP_DIR}/helmv3"
-mv "${TMP_DIR}/helmv3/${PLATFORM}-${ARCH}/helm" "${TOOLS_DIR}/helmv3"
+## Install helm
+mkdir -p "${TMP_DIR}/helm"
+curl -sSL https://get.helm.sh/helm-${HELM_VERSION}-${PLATFORM}-${ARCH}.tar.gz | tar xz -C "${TMP_DIR}/helm"
+mv "${TMP_DIR}/helm/${PLATFORM}-${ARCH}/helm" "${TOOLS_DIR}/helm"
 rm -rf "${PLATFORM}-${ARCH}"
 
 ## Install Bats
 curl -sSL https://github.com/bats-core/bats-core/archive/v${BATS_VERSION}.tar.gz | tar xz -C "${TOOLS_DIR}"
 ln -s ${TOOLS_DIR}/bats-core-${BATS_VERSION}/bin/bats ${TOOLS_DIR}/bats
-
-## Initialize helm
-
-helmv2 init --stable-repo-url https://charts.helm.sh/stable --client-only --kubeconfig="${BUILD_DIR}/.kube/kubeconfig"
-
 
 ## Install kind
 curl -sSL "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-${PLATFORM}-${ARCH}" -o "${TOOLS_DIR}/kind"
