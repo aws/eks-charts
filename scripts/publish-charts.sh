c2cbab3c6403c66997e76dec59ea0f32d439e87e
Zachary Nixon <nixozach@amazon.com>
2025-03-17 10:10:38 -0700

    Add debug logging to publish script

--- scripts/publish-charts.sh
+++ scripts/publish-charts.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 
 GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
 BUILD_DIR="${GIT_REPO_ROOT}/build"
@@ -13,7 +14,10 @@ if echo "${VERSION}" | grep -Eq "^v[0-9]+(\.[0-9]+){2}$"; then
     git fetch --all
     git config user.email eks-bot@users.noreply.github.com
     git config user.name eks-bot
+    # Don't leak GITHUB_TOKEN
+    set +x
     git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}
+    set -x
     git config pull.rebase false
     git checkout gh-pages
     mv -n $PACKAGE_DIR/stable/*.tgz .
