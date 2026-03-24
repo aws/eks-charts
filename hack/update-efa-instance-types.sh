6d0367691bb151ffefca680d34df922725ad3e27
mselim00 <100049111+mselim00@users.noreply.github.com>
2026-03-23 17:32:25 -0700

    chore: fix efa update script consistency (#1311)

--- hack/update-efa-instance-types.sh
+++ hack/update-efa-instance-types.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# This script requires yq >= v4, install it here:
+# This script requires yq >= v4.52, install it here:
 # https://github.com/mikefarah/yq/?tab=readme-ov-file#install
+
+# Hard code just preview instance types or ones that may not show up in Describe responses
+ALL_TYPES=("p6e-gb300.36xlarge" "p6e-gb200.36xlarge" "trn2-ac.24xlarge" "trn2u-ac.24xlarge" "trn2u.48xlarge")
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 EFA_CHART_DIRECTORY="${PROJECT_ROOT}/stable/aws-efa-k8s-device-plugin"
-
-CHART_FILE="${EFA_CHART_DIRECTORY}/Chart.yaml"
-if git diff --quiet --exit-code -- "$CHART_FILE" || \
-   ! git diff "$CHART_FILE" | grep -q '^[-+]version:'; then
-    # Increment patch version if version hasn't already been incremented
-    VERSION=$(yq eval '.version' "$CHART_FILE")
-    BARE="${VERSION#v}"
-    IFS='.' read -r MAJOR MINOR PATCH <<< "$BARE"
-    NEW_VERSION="v${MAJOR}.${MINOR}.$((PATCH + 1))"
-    yq eval -i ".version = \"${NEW_VERSION}\"" "$CHART_FILE"
-else
-    echo "Version already changed; skipping increment"
-fi
 
 # Get list of opted-in regions
 mapfile -t REGIONS < <(
@@ -27,9 +17,6 @@ mapfile -t REGIONS < <(
     --output text \
   | tr '\t' '\n'
 )
-
-# Hard code just preview instance types or ones that may not show up in Describe responses
-ALL_TYPES=("p6e-gb200.36xlarge" "trn2-ac.24xlarge" "trn2u-ac.24xlarge" "trn2u.48xlarge")
 
 # Fetch instance types from each region
 for REGION in "${REGIONS[@]}"; do
@@ -46,8 +33,19 @@ for REGION in "${REGIONS[@]}"; do
   ALL_TYPES+=("${TYPES[@]}")
 done
 
-# Deduplicate + sort, then build a yq array expression
-YQ_ARRAY=$(printf '%s\n' "${ALL_TYPES[@]}" | sort -u | sed 's/.*/"&"/' | paste -sd, -)
+# Build a yq array, then deduplicate + sort with yq builtins
+export YQ_ARRAY=$(printf -- '- %s\n' "${ALL_TYPES[@]}" | yq 'unique | sort')
 
 VALUES_FILE="${EFA_CHART_DIRECTORY}/values.yaml"
-yq eval -i ".supportedInstanceLabels.values = [${YQ_ARRAY}]" "$VALUES_FILE"
+yq eval -i '.supportedInstanceLabels.values = env(YQ_ARRAY)' "$VALUES_FILE"
+
+CHART_FILE="${EFA_CHART_DIRECTORY}/Chart.yaml"
+if ! git diff --quiet --exit-code -- "${EFA_CHART_DIRECTORY}/values.yaml" && \
+   ! git diff "$CHART_FILE" | grep -q '^[-+]version:'; then
+    # Increment patch version if version hasn't already been incremented
+    VERSION=$(yq eval '.version' "$CHART_FILE")
+    BARE="${VERSION#v}"
+    IFS='.' read -r MAJOR MINOR PATCH <<< "$BARE"
+    NEW_VERSION="v${MAJOR}.${MINOR}.$((PATCH + 1))"
+    yq eval -i ".version = \"${NEW_VERSION}\"" "$CHART_FILE"
+fi
